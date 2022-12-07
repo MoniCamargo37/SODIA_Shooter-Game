a56bcc38460f1d2f3eb5df27496a2154315ca15e
MoniCamargo37 <moni28ac@gmail.com>
2022-12-07 20:23:26 +0100

    done

--- js/assets.js
+++ js/assets.js
@@ -1,50 +1,50 @@
 const lostPagesong = new Audio();
-lostPagesong.src = '../img/GameOver.mp3';
+lostPagesong.src = '/img/GameOver.mp3';
 
 const winPagesong = new Audio();
-winPagesong.src = '../img/winSong.mp3';
+winPagesong.src = '/img/winSong.mp3';
 
 const gamePageSong = new Audio();
-gamePageSong.src = '../img/Gamesong.mp3';
+gamePageSong.src = '/img/Gamesong.mp3';
 
 let shotEffect = new Audio();
-shotEffect.src = '../img/shot.mp3';
+shotEffect.src = '/img/shot.mp3';
 
 const warriorDown = new Image();
-warriorDown.src= '../img/heroDown.png';
+warriorDown.src= '/img/heroDown.png';
 
 const warriorUp = new Image();
-warriorUp.src= '../img/heroUp.png';
+warriorUp.src= '/img/heroUp.png';
 
 const warriorRight = new Image();
-warriorRight.src= '../img/heroRight.png';
+warriorRight.src= '/img/heroRight.png';
 
 const warriorLeft = new Image();
-warriorLeft.src= '../img/heroLeft.png';
+warriorLeft.src= '/img/heroLeft.png';
 
 const enemyNorth1 = new Image();
-enemyNorth1.src= '../img/enemyNorth1.png';
+enemyNorth1.src= '/img/enemyNorth1.png';
 
 const enemyEast1 = new Image();
-enemyEast1.src= '../img/enemyEast1.png';
+enemyEast1.src= '/img/enemyEast1.png';
 
 const enemySouth1 = new Image();
-enemySouth1.src= '../img/enemySouth1.png';
+enemySouth1.src= '/img/enemySouth1.png';
 
 const enemyWest1 = new Image();
-enemyWest1.src= '../img/enemyWest1.png';
+enemyWest1.src= '/img/enemyWest1.png';
 
 const enemyNorth2 = new Image();
-enemyNorth2.src= '../img/wolfSouth.png';
+enemyNorth2.src= '/img/wolfSouth.png';
 
 const enemySouth2 = new Image();
-enemySouth2.src= '../img/wolfNorth.png';
+enemySouth2.src= '/img/wolfNorth.png';
 
 const enemyWest2 = new Image();
-enemyWest2.src= '../img/wolfWest.png';
+enemyWest2.src= '/img/wolfWest.png';
 
 const enemyEast2 = new Image();
-enemyEast2.src= '../img/wolfEast.png';
+enemyEast2.src= '/img/wolfEast.png';
 
 const enemyDragon = new Image();
-enemyDragon.src = '../img/enemyDragon.png';
+enemyDragon.src = '/img/enemyDragon.png';
